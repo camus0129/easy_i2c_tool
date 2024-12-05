1b92daffd2bead87e4f79c1a341874e43751aed2
Sven <camus0129@live.com>
2024-12-05 11:36:07 +0800

    added bus num paramter and optimize some log msg

--- amp_i2c_test.c
+++ amp_i2c_test.c
@@ -11,14 +11,22 @@
 #include<linux/i2c.h>
 #include<linux/i2c-dev.h>
 
-#define DEV_BUS "/dev/i2c-2"
+#define BUS_NUM_PARAM 1
+#define READ_WRITE_PARAM 2
+#define DEVICE_ID  3
+#define REGADDR 4
+#define READ_LEN 5
+#define VALUE_START 5
+
 static int amp_fd;
-int i2c_init(char *dev_bus)
+int i2c_init(int bus_num)
 {
-	int i2c_fd = open(dev_bus, O_RDWR);
+	char buf[20]={0};
+	sprintf(buf,"/dev/i2c-%d",bus_num);
+	int i2c_fd = open(buf, O_RDWR);
 	if (i2c_fd < 0)
 	{
-		printf("open %s fail\n",dev_bus);
+		printf("open %s fail\n",buf);
 		return -1;
 	}
 	return i2c_fd;
@@ -69,44 +77,58 @@ int i2c_write_byte(unsigned char chip_addr,unsigned char reg_addr,unsigned char 
 {
 	return i2c_write_buf(chip_addr,reg_addr,&value,1);
 }
+void help_info(void)
+{
+	printf("Useage:\n");
+	printf("\n1. read write directly:\n");
+	printf("amp_i2c_test bus-num w/r device regaddr value/length\n");
+	printf("example:\n");
+	printf("amp_i2c_test 2 w 0x58 0x00 0x01 //write dev=0x58 reg=0x00,value=0x01 in  /dev/i2c-2 \n");
+	printf("amp_i2c_test 2 r 0x58 0x00 0x01 //read dev=0x58 reg=0x00,lenth=0x01 in /dev/i2c-2\n");
+	printf("\n2. load read/write cmd from batch file:\n");
+	printf("amp_i2c_test bus-num -f cmd batch file\n");
+}
 int main(int argc,char *argv[])
 {
 	if(argc < 4)
 	{
-		printf("Useage:amp_i2c_test w/r device regaddr value\n");
+		help_info();
 		return 0;
 	}
-	amp_fd = i2c_init(DEV_BUS); 
+	amp_fd = i2c_init(strtoul(argv[BUS_NUM_PARAM],NULL,10)); 
 	if(amp_fd < 0)
 	{
 		return -1;
 	}
-	unsigned char device_id = strtoul(argv[2],NULL,16)/2;
-	unsigned char regaddr = strtoul(argv[3],NULL,16);
-	printf("deviceid = 0x%02x regaddr = 0x%02x\n",device_id,regaddr);
-	if(!strcmp(argv[1],"w"))
+	unsigned char device_id = strtoul(argv[DEVICE_ID],NULL,16)/2;
+	unsigned char regaddr = strtoul(argv[REGADDR],NULL,16);
+	//printf("deviceid = 0x%02x regaddr = 0x%02x\n",device_id,regaddr);
+	if(!strcmp(argv[READ_WRITE_PARAM],"w"))
 		{
-			printf("write cmd\n");
-			unsigned value_len = argc - 4;
+			//printf("write cmd\n");
+			unsigned value_len = argc - VALUE_START;
 			unsigned char *buf=malloc(value_len);
+			printf("write regaddr 0x%02x value = ",regaddr);
 			for(int i=0;i<value_len;i++)
 			{
-				buf[i] = strtoul(argv[i+4],NULL,16);
-				printf(" write value[%d]=0x%02x\n",i,buf[i]);
+				buf[i] = strtoul(argv[i+VALUE_START],NULL,16);
+				printf("0x%02x ",buf[i]);
 			}
+			printf("\n");
 			i2c_write_buf(device_id,regaddr,buf,value_len);
 		}
-	else if(!strcmp(argv[1],"r"))
+	else if(!strcmp(argv[READ_WRITE_PARAM],"r"))
 	{
-		printf("read cmd\n");
-		unsigned value_len = strtoul(argv[4],NULL,16);
+		//printf("read cmd\n");
+		unsigned value_len = strtoul(argv[READ_LEN],NULL,16);
 		unsigned char *buf = malloc(value_len);
-		printf("read len = %d\n",value_len);
+		printf("read regaddr 0x%02x value = ",regaddr);
 		i2c_read_buf(device_id,regaddr,buf,value_len);
 		for(int i=0;i<value_len;i++)
 		{
-			printf("read value[%d]=0x%02x\n",i,buf[i]);
+			printf("0x%02x ",buf[i]);
 		}
+		printf("\n");
 	}
 	else
 	{
